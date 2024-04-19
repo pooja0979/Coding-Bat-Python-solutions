8b0248f6a08166454668d754d5a11e21cdf696c3
pooja0979 <67052915+pooja0979@users.noreply.github.com>
2024-04-19 13:05:45 +0200

    First commit

---
+++ Section 2 Practice Tasks.py
@@ -0,0 +1,5 @@
+#Task 1 Food and Colour
+color=input("Whatis your favourite colour?")
+food=input("What is your favourite food?")
+time=input("When do you need it - Breakfast, Lunch or dinner?")
+print(f"Yu! I'll have {color} {food} for {time}")
