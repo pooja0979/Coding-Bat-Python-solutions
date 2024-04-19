561633e452dcc94f56751e1a3d8fd73259616ffc
pooja0979 <67052915+pooja0979@users.noreply.github.com>
2024-04-19 13:28:23 +0200

    second commit

--- Section 2 Practice Tasks.py
+++ Section 2 Practice Tasks.py
@@ -1,5 +1,28 @@
 #Task 1 Food and Colour
-color=input("Whatis your favourite colour?")
+'''color=input("Whatis your favourite colour?")
 food=input("What is your favourite food?")
 time=input("When do you need it - Breakfast, Lunch or dinner?")
-print(f"Yu! I'll have {color} {food} for {time}")
+print(f"You! I'll have {color} {food} for {time}")
+'''
+
+#Task 2
+
+country=["Botswana","France","Germany","Hungary"]
+capital=["Gaborne","Paris","Berlin","Budapest"]
+#for i in range(0,len(country)-1):
+ask=""
+while True and ask!="Exit":
+        num=int(input("enter the index value of the country"))
+        if 0 <= num <= 3:
+                print(capital[num])
+                ask=input("Would you like to go ahead or Exit?")
+        else:
+                print("Sorry, wrong input. Index value must be between 0 and 3.")
+                ask=input("Would you like to go ahead or Exit?")
+        
+        
+                
+        
+                
+        
+ 
